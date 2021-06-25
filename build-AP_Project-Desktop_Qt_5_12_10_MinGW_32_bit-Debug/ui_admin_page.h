16e193d6bcb81df060d8d35a4ca20f546decf2dc
alireza rahmani <alir.rah81@gmail.com>
2021-06-25 16:07:54 +0430

    design admin_page.iu

--- build-AP_Project-Desktop_Qt_5_12_10_MinGW_32_bit-Debug/ui_admin_page.h
+++ build-AP_Project-Desktop_Qt_5_12_10_MinGW_32_bit-Debug/ui_admin_page.h
@@ -11,6 +11,20 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -18,14 +32,353 @@ QT_BEGIN_NAMESPACE
 class Ui_admin_page
 {
 public:
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QHBoxLayout *horizontalLayout_2;
+    QTabWidget *tabWidget_2;
+    QWidget *tab_4;
+    QVBoxLayout *verticalLayout_3;
+    QSpacerItem *verticalSpacer;
+    QGroupBox *groupBox;
+    QVBoxLayout *verticalLayout_4;
+    QComboBox *comboBox;
+    QLineEdit *lineEdit;
+    QSpacerItem *verticalSpacer_2;
+    QGroupBox *groupBox_2;
+    QVBoxLayout *verticalLayout_5;
+    QRadioButton *radioButton;
+    QRadioButton *radioButton_2;
+    QRadioButton *radioButton_3;
+    QRadioButton *radioButton_4;
+    QRadioButton *radioButton_5;
+    QSpacerItem *verticalSpacer_3;
+    QWidget *tab_5;
+    QVBoxLayout *verticalLayout_7;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_2;
+    QLineEdit *lineEdit_2;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label_3;
+    QLineEdit *lineEdit_3;
+    QHBoxLayout *horizontalLayout_5;
+    QLabel *label_4;
+    QLineEdit *lineEdit_4;
+    QHBoxLayout *horizontalLayout_6;
+    QLabel *label_5;
+    QLineEdit *lineEdit_5;
+    QHBoxLayout *horizontalLayout_12;
+    QLabel *label_10;
+    QLineEdit *lineEdit_11;
+    QSpacerItem *verticalSpacer_4;
+    QGridLayout *gridLayout;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QVBoxLayout *verticalLayout_2;
+    QTableWidget *tableWidget;
+    QWidget *tab_2;
+    QHBoxLayout *horizontalLayout_7;
+    QGroupBox *groupBox_3;
+    QVBoxLayout *verticalLayout_8;
+    QTextEdit *textEdit;
+    QGridLayout *gridLayout_2;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_7;
+    QGroupBox *groupBox_4;
+    QVBoxLayout *verticalLayout_6;
+    QTableWidget *tableWidget_2;
 
     void setupUi(QWidget *admin_page)
     {
         if (admin_page->objectName().isEmpty())
             admin_page->setObjectName(QString::fromUtf8("admin_page"));
-        admin_page->resize(400, 300);
+        admin_page->resize(1023, 631);
+        verticalLayout = new QVBoxLayout(admin_page);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        label = new QLabel(admin_page);
+        label->setObjectName(QString::fromUtf8("label"));
+        QFont font;
+        font.setPointSize(10);
+        label->setFont(font);
+
+        horizontalLayout->addWidget(label);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+        pushButton_2 = new QPushButton(admin_page);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setFont(font);
+
+        horizontalLayout->addWidget(pushButton_2);
+
+        pushButton = new QPushButton(admin_page);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setFont(font);
+
+        horizontalLayout->addWidget(pushButton);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        tabWidget = new QTabWidget(admin_page);
+        tabWidget->setObjectName(QString::fromUtf8("tabWidget"));
+        tabWidget->setFont(font);
+        tabWidget->setTabPosition(QTabWidget::West);
+        tab = new QWidget();
+        tab->setObjectName(QString::fromUtf8("tab"));
+        horizontalLayout_2 = new QHBoxLayout(tab);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        tabWidget_2 = new QTabWidget(tab);
+        tabWidget_2->setObjectName(QString::fromUtf8("tabWidget_2"));
+        tab_4 = new QWidget();
+        tab_4->setObjectName(QString::fromUtf8("tab_4"));
+        verticalLayout_3 = new QVBoxLayout(tab_4);
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_3->addItem(verticalSpacer);
+
+        groupBox = new QGroupBox(tab_4);
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
+        verticalLayout_4 = new QVBoxLayout(groupBox);
+        verticalLayout_4->setObjectName(QString::fromUtf8("verticalLayout_4"));
+        comboBox = new QComboBox(groupBox);
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+
+        verticalLayout_4->addWidget(comboBox);
+
+
+        verticalLayout_3->addWidget(groupBox);
+
+        lineEdit = new QLineEdit(tab_4);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+
+        verticalLayout_3->addWidget(lineEdit);
+
+        verticalSpacer_2 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_3->addItem(verticalSpacer_2);
+
+        groupBox_2 = new QGroupBox(tab_4);
+        groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
+        verticalLayout_5 = new QVBoxLayout(groupBox_2);
+        verticalLayout_5->setObjectName(QString::fromUtf8("verticalLayout_5"));
+        radioButton = new QRadioButton(groupBox_2);
+        radioButton->setObjectName(QString::fromUtf8("radioButton"));
+
+        verticalLayout_5->addWidget(radioButton);
+
+        radioButton_2 = new QRadioButton(groupBox_2);
+        radioButton_2->setObjectName(QString::fromUtf8("radioButton_2"));
+
+        verticalLayout_5->addWidget(radioButton_2);
+
+        radioButton_3 = new QRadioButton(groupBox_2);
+        radioButton_3->setObjectName(QString::fromUtf8("radioButton_3"));
+
+        verticalLayout_5->addWidget(radioButton_3);
+
+        radioButton_4 = new QRadioButton(groupBox_2);
+        radioButton_4->setObjectName(QString::fromUtf8("radioButton_4"));
+
+        verticalLayout_5->addWidget(radioButton_4);
+
+        radioButton_5 = new QRadioButton(groupBox_2);
+        radioButton_5->setObjectName(QString::fromUtf8("radioButton_5"));
+
+        verticalLayout_5->addWidget(radioButton_5);
+
+
+        verticalLayout_3->addWidget(groupBox_2);
+
+        verticalSpacer_3 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_3->addItem(verticalSpacer_3);
+
+        tabWidget_2->addTab(tab_4, QString());
+        tab_5 = new QWidget();
+        tab_5->setObjectName(QString::fromUtf8("tab_5"));
+        verticalLayout_7 = new QVBoxLayout(tab_5);
+        verticalLayout_7->setObjectName(QString::fromUtf8("verticalLayout_7"));
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        horizontalLayout_3->setContentsMargins(-1, -1, 0, -1);
+        label_2 = new QLabel(tab_5);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        horizontalLayout_3->addWidget(label_2);
+
+        lineEdit_2 = new QLineEdit(tab_5);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+
+        horizontalLayout_3->addWidget(lineEdit_2);
+
+
+        verticalLayout_7->addLayout(horizontalLayout_3);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        label_3 = new QLabel(tab_5);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        horizontalLayout_4->addWidget(label_3);
+
+        lineEdit_3 = new QLineEdit(tab_5);
+        lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
+
+        horizontalLayout_4->addWidget(lineEdit_3);
+
+
+        verticalLayout_7->addLayout(horizontalLayout_4);
+
+        horizontalLayout_5 = new QHBoxLayout();
+        horizontalLayout_5->setObjectName(QString::fromUtf8("horizontalLayout_5"));
+        label_4 = new QLabel(tab_5);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        horizontalLayout_5->addWidget(label_4);
+
+        lineEdit_4 = new QLineEdit(tab_5);
+        lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
+
+        horizontalLayout_5->addWidget(lineEdit_4);
+
+
+        verticalLayout_7->addLayout(horizontalLayout_5);
+
+        horizontalLayout_6 = new QHBoxLayout();
+        horizontalLayout_6->setObjectName(QString::fromUtf8("horizontalLayout_6"));
+        label_5 = new QLabel(tab_5);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+
+        horizontalLayout_6->addWidget(label_5);
+
+        lineEdit_5 = new QLineEdit(tab_5);
+        lineEdit_5->setObjectName(QString::fromUtf8("lineEdit_5"));
+
+        horizontalLayout_6->addWidget(lineEdit_5);
+
+
+        verticalLayout_7->addLayout(horizontalLayout_6);
+
+        horizontalLayout_12 = new QHBoxLayout();
+        horizontalLayout_12->setObjectName(QString::fromUtf8("horizontalLayout_12"));
+        label_10 = new QLabel(tab_5);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+
+        horizontalLayout_12->addWidget(label_10);
+
+        lineEdit_11 = new QLineEdit(tab_5);
+        lineEdit_11->setObjectName(QString::fromUtf8("lineEdit_11"));
+
+        horizontalLayout_12->addWidget(lineEdit_11);
+
+
+        verticalLayout_7->addLayout(horizontalLayout_12);
+
+        verticalSpacer_4 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_7->addItem(verticalSpacer_4);
+
+        gridLayout = new QGridLayout();
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        pushButton_3 = new QPushButton(tab_5);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+
+        gridLayout->addWidget(pushButton_3, 0, 0, 1, 1);
+
+        pushButton_4 = new QPushButton(tab_5);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+
+        gridLayout->addWidget(pushButton_4, 0, 1, 1, 1);
+
+        pushButton_5 = new QPushButton(tab_5);
+        pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
+
+        gridLayout->addWidget(pushButton_5, 1, 0, 1, 2);
+
+
+        verticalLayout_7->addLayout(gridLayout);
+
+        tabWidget_2->addTab(tab_5, QString());
+
+        horizontalLayout_2->addWidget(tabWidget_2);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        tableWidget = new QTableWidget(tab);
+        tableWidget->setObjectName(QString::fromUtf8("tableWidget"));
+
+        verticalLayout_2->addWidget(tableWidget);
+
+
+        horizontalLayout_2->addLayout(verticalLayout_2);
+
+        tabWidget->addTab(tab, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QString::fromUtf8("tab_2"));
+        horizontalLayout_7 = new QHBoxLayout(tab_2);
+        horizontalLayout_7->setObjectName(QString::fromUtf8("horizontalLayout_7"));
+        groupBox_3 = new QGroupBox(tab_2);
+        groupBox_3->setObjectName(QString::fromUtf8("groupBox_3"));
+        verticalLayout_8 = new QVBoxLayout(groupBox_3);
+        verticalLayout_8->setObjectName(QString::fromUtf8("verticalLayout_8"));
+        textEdit = new QTextEdit(groupBox_3);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+
+        verticalLayout_8->addWidget(textEdit);
+
+        gridLayout_2 = new QGridLayout();
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        gridLayout_2->setContentsMargins(-1, -1, -1, 0);
+        pushButton_6 = new QPushButton(groupBox_3);
+        pushButton_6->setObjectName(QString::fromUtf8("pushButton_6"));
+
+        gridLayout_2->addWidget(pushButton_6, 0, 0, 1, 1);
+
+        pushButton_7 = new QPushButton(groupBox_3);
+        pushButton_7->setObjectName(QString::fromUtf8("pushButton_7"));
+
+        gridLayout_2->addWidget(pushButton_7, 0, 1, 1, 1);
+
+
+        verticalLayout_8->addLayout(gridLayout_2);
+
+
+        horizontalLayout_7->addWidget(groupBox_3);
+
+        groupBox_4 = new QGroupBox(tab_2);
+        groupBox_4->setObjectName(QString::fromUtf8("groupBox_4"));
+        verticalLayout_6 = new QVBoxLayout(groupBox_4);
+        verticalLayout_6->setObjectName(QString::fromUtf8("verticalLayout_6"));
+        tableWidget_2 = new QTableWidget(groupBox_4);
+        tableWidget_2->setObjectName(QString::fromUtf8("tableWidget_2"));
+
+        verticalLayout_6->addWidget(tableWidget_2);
+
+
+        horizontalLayout_7->addWidget(groupBox_4);
+
+        tabWidget->addTab(tab_2, QString());
+
+        verticalLayout->addWidget(tabWidget);
+
 
         retranslateUi(admin_page);
+
+        tabWidget->setCurrentIndex(0);
+        tabWidget_2->setCurrentIndex(1);
+
 
         QMetaObject::connectSlotsByName(admin_page);
     } // setupUi
@@ -33,6 +386,32 @@ public:
     void retranslateUi(QWidget *admin_page)
     {
         admin_page->setWindowTitle(QApplication::translate("admin_page", "Form", nullptr));
+        label->setText(QApplication::translate("admin_page", "TextLabel", nullptr));
+        pushButton_2->setText(QApplication::translate("admin_page", "PushButton", nullptr));
+        pushButton->setText(QApplication::translate("admin_page", "PushButton", nullptr));
+        groupBox->setTitle(QApplication::translate("admin_page", "GroupBox", nullptr));
+        groupBox_2->setTitle(QApplication::translate("admin_page", "GroupBox", nullptr));
+        radioButton->setText(QApplication::translate("admin_page", "RadioButton", nullptr));
+        radioButton_2->setText(QApplication::translate("admin_page", "RadioButton", nullptr));
+        radioButton_3->setText(QApplication::translate("admin_page", "RadioButton", nullptr));
+        radioButton_4->setText(QApplication::translate("admin_page", "RadioButton", nullptr));
+        radioButton_5->setText(QApplication::translate("admin_page", "RadioButton", nullptr));
+        tabWidget_2->setTabText(tabWidget_2->indexOf(tab_4), QApplication::translate("admin_page", "Tab 1", nullptr));
+        label_2->setText(QApplication::translate("admin_page", "TextLabel", nullptr));
+        label_3->setText(QApplication::translate("admin_page", "TextLabel", nullptr));
+        label_4->setText(QApplication::translate("admin_page", "TextLabel", nullptr));
+        label_5->setText(QApplication::translate("admin_page", "TextLabel", nullptr));
+        label_10->setText(QApplication::translate("admin_page", "TextLabel", nullptr));
+        pushButton_3->setText(QApplication::translate("admin_page", "PushButton", nullptr));
+        pushButton_4->setText(QApplication::translate("admin_page", "PushButton", nullptr));
+        pushButton_5->setText(QApplication::translate("admin_page", "PushButton", nullptr));
+        tabWidget_2->setTabText(tabWidget_2->indexOf(tab_5), QApplication::translate("admin_page", "Tab 2", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("admin_page", "Tab 1", nullptr));
+        groupBox_3->setTitle(QApplication::translate("admin_page", "GroupBox", nullptr));
+        pushButton_6->setText(QApplication::translate("admin_page", "PushButton", nullptr));
+        pushButton_7->setText(QApplication::translate("admin_page", "PushButton", nullptr));
+        groupBox_4->setTitle(QApplication::translate("admin_page", "GroupBox", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("admin_page", "Tab 2", nullptr));
     } // retranslateUi
 
 };
